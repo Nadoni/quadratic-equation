fc6c9f4ef2c72654ab0e3e3bd8bab6f4d0387590
kojaniydracon <kojaniydracon@gmail.com>
2018-02-25 18:01:43 +0300

    quadratisch practisch Gut

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+    let components = equation.split(' ');
+    let a = components[0];
+    let b = components.slice(3, 5).join('');
+    let c = components.slice(7).join('');
+    let D = b*b - 4 * a * c;
+    let x1 = (-b + Math.sqrt(D))/(2*a);
+    let x2 = (-b - Math.sqrt(D))/(2*a);
+    console.log(components);
+    console.log(Math.min(x1,x2), Math.max(x1,x2));
+
+    return [Math.round(Math.min(x1,x2)), Math.round(Math.max(x1,x2))];
+};
